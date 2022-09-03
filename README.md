dc58955e4380222bfea9a99628c3ad11c11ecdb1
Keshav Gupta <59222869+keshavgupta848101@users.noreply.github.com>
2022-09-03 01:33:58 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# markOne: “Do you know Mahabharat?” CLI quiz app
+# About Quiz
+1. Take Username as input.
+2. 5 questions for user to play.
+3. Display the final score to the user
+
+# Main Screen
+
+![image](https://res.cloudinary.com/debo7pflq/image/upload/v1662166780/github/1_dxie9a.png)
+
+# Game Screen
+
+![image](https://res.cloudinary.com/debo7pflq/image/upload/v1662166942/github/Screenshot_1490_zvyzsf.png)
+
+# Play Link
+[Game Link](https://replit.com/@keshavgupta8481/cli-do-you-know-me-quiz?embed=true)
---
+++ index.js
@@ -0,0 +1,88 @@
+import readlineSync from "readline-sync";
+import chalk from 'chalk'
+var score = 0;
+
+var highScores = [
+  {
+    name: "Ravi",
+    score: 1,
+  },
+
+  {
+    name: "Deepak",
+    score: 3,
+  },
+  {
+    name: "Raju",
+    score: 1,
+  },
+  {
+    name: "Shiv",
+    score: 4,
+  },
+]
+
+// array of objects
+var questionArray = [
+  {
+    question: "Who shot the first arrow that killed Bheeshma pitama?",
+    answer: "Steve Rogers"
+  }, {
+    question: "Who is the well known charioteer of Dhritarashtra?",
+    answer: ""
+  },
+  {
+    question: "Who was the real father of Arjuna?",
+    answer: "Indra"
+  },
+  {
+    question: "Who is Pandu's oldest son?",
+    answer: "Yudhisthira"
+  },
+  {
+    question: "What famous text does Krsna dictate to Arjuna?",
+    answer: "Bhagwad gita"
+  }
+];
+
+function welcomeUser() {
+  var firstName = readlineSync.question("Namaste! Please enter your name:");
+  console.log(chalk.blue(firstName + " Apka swagat hai"));
+}
+
+function compareAnswer(question, answer) {
+  var userAnswer = readlineSync.question(question);
+
+  if (userAnswer.toUpperCase() === answer.toUpperCase()) {
+    console.log(chalk.green("right!"));
+    score = score + 1;
+
+  } else {
+    console.log(chalk.red("Wrong!"));
+
+  }
+
+  console.log(chalk.whiteBright("Score " + score));
+  console.log("-------------")
+}
+
+
+function startTheGame() {
+  for (let i = 0; i < questionArray.length; i++) {
+    compareAnswer(questionArray[i].question, questionArray[i].answer)
+  }
+
+}
+
+function showScore() {
+  console.log("YAY! You SCORED: ", score);
+
+  console.log("Check out the high scores, if you should be there ping me and I'll update it");
+
+  highScores.map(score => console.log(score.name, " : ", score.score))
+}
+
+
+welcomeUser();
+startTheGame();
+showScore();
